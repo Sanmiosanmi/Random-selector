c89e367387a3dc1d9164b140a3006a8b3355d2b7
olasunkanmi ilesanmi <sunkyx99@gmail.com>
2020-03-30 21:56:36 +0100

    setup production build and server

--- src/Components/Options.js
+++ src/Components/Options.js
@@ -12,7 +12,7 @@ const Options = (props) => (
     </div>
     {props.options.length === 0 && <p className='widgets-p'>Please add an option to get started!</p>}
     {
-      props.options.map((option,index) => (
+      props.options.map((option, index) => (
         <Option
           key={option}
           optionText={option}
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,40 @@
 const path = require('path');
+ 
+module.exports = (env) =>{
+  const isProduction = env === 'production';
+  console.log('env', env);
+  return{
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    },  
-     {
-       test: /\.s?css$/,
-       use: [
-      'style-loader',
-      'css-loader',
-      'sass-loader'
+ 
+      entry: './src/app.js',
+      output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+      },
+      module: {
+        rules: [{
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },  
+         {
+           test: /\.s?css$/,
+           use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+    
+           ]
+         }]
+      },
+      devtool: 'cheap-module-eval-source-map',
+      devServer: {
+        contentBase: path.join(__dirname, 'public')
+      }
+      
+    }
+    
 
-       ]
-     }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
-  }
-  
-};
+  };
+
+
